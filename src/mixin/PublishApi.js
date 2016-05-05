967b0d65eb4210c357508c520e3d438d7765701f
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-05-05 11:52:52 +0200

    A utility mixin to publish ViewController API to View

---
+++ src/mixin/PublishApi.js
@@ -0,0 +1,31 @@
+//Disable some of the JSLint warnings
+/*global window,console,Ext*/
+(function(){
+    //Make sure strict mode is on
+    'use strict';
+    
+    /**
+     * Meant to be used as a mixin
+     */
+    Ext.define('mh.mixin.PublishApi', {
+        /**
+         * @param {string[]} arguments
+         * publishes ViewController API, so it is accessible from a View level
+         */
+        publishApi: function(){
+            var apis = Ext.isArray(arguments[0]) ? arguments[0] : arguments,
+                view = this.getView(),
+                method,
+                a = 0, alen = apis.length;
+
+            for(a; a < alen; a++){
+                method = arguments[a];
+
+                if(!view[method] && Ext.isFunction(this[method])){
+                    view[method] = Ext.bind(this[method], this);
+                }
+            }
+        }
+    });
+
+}());
