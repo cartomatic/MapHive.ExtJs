5b2fd16b38442f389330c294c2acce01da6b50a7
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-05-06 13:48:03 +0200

    Add web.config overridable api map

--- src/mixin/ApiMap.js
+++ src/mixin/ApiMap.js
@@ -3,7 +3,16 @@
 (function(){
     //Make sure strict mode is on
     'use strict';
-    
+
+    var apiMapConfigured = false,
+
+        //Note: this is overridable through the web.config... see the MhApiMap key for details
+        apiMap = {
+            applications: 'applications',
+
+            tokenValidation: 'auth/tokenvalidation'
+        };
+
     /**
      * Provides a centralised access point to the API endpoints
      */
@@ -13,15 +22,42 @@
             'mh.mixin.InitialCfg'
         ],
 
-        //Note: it could be desirable to make it customisable externally...
-        apiMap: {
-            applications: 'GetApps.json',
+        /**
+         * Whether or not the API map has already been configured
+         */
+        apiMapConfigured: false,
 
-            tokenValidation: 'ValidateToken.json'//'auth/tokenvalidation'
-        },
+
 
         getApiEndPoint: function(endPoint){
-            return this.getMhCfgProperty('mhApiEndPoint') + this.apiMap[endPoint];
+
+            if(!apiMapConfigured){
+                this.configureApiMap();
+            }
+
+            return this.getMhCfgProperty('mhApiEndPoint') + apiMap[endPoint];
+        },
+
+        /**
+         * Overrides the default API map with the configuration defined in the web.config
+         */
+        configureApiMap: function(){
+
+            //grab the cfg off the global initial cfg
+            var apiMapChanges = this.getMhCfgProperty('mhApiMap'),
+                keys, key, k = 0, klen;
+            if(apiMapChanges){
+                keys = Ext.Object.getKeys(apiMapChanges);
+                klen = keys.length;
+
+                for(k; k < klen; k++){
+                    key = keys[k];
+
+                    apiMap[key] = apiMapChanges[key];
+                }
+            }
+
+            apiMapConfigured = true;
         }
 
     });
