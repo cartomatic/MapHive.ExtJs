ecf1eb20c591ba5ddec390f5db52fed33c4776b9
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-28 13:20:36 +0200

    simple null identifier

---
+++ src/data/identifier/Null.js
@@ -0,0 +1,20 @@
+//Disable some of the JSLint warnings
+/*global window,console,Ext*/
+(function(){
+    //Make sure strict mode is on
+    'use strict';
+    
+    /**
+     * A simple null id generator, so can enforce null ids on new models
+     */
+    Ext.define('mh.data.identifier.Null', {
+        extend: 'Ext.data.identifier.Generator',
+
+        alias: 'data.identifier.mhnull',
+
+        generate: function(){
+            return null;
+        }
+    });
+
+}());
