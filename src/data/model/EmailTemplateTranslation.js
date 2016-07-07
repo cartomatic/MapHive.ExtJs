aca6feb6fadf35324c864084e2a788235bbc8740
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-07-07 16:29:43 +0200

    Model rename

--- src/data/model/EmailTemplateTranslation.js
+++ src/data/model/EmailTemplateTranslation.js
@@ -4,13 +4,14 @@
     //Make sure strict mode is on
     'use strict';
 
-    Ext.define('mh.data.model.EmailTemplate', {
+    Ext.define('mh.data.model.EmailTemplateTranslation', {
         extend: 'Ext.data.Model',
 
         requires: [
         ],
 
         fields: [
+            {name: 'langCode', type: 'string'},
             {name: 'title', type: 'string'},
             {name: 'body', type: 'string'}
         ]
