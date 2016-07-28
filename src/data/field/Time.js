877dcae101639de9bfd906fc3c7ef5b5928f26b9
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-07-28 05:08:55 +0200

    Add a data.model.field.Time

---
+++ src/data/field/Time.js
@@ -0,0 +1,33 @@
+(function(){
+    //Make sure strict mode is on
+    'use strict';
+    
+    /**
+     * Created by info_000 on 21-Jul-16.
+     */
+    Ext.define('mh.data.field.Time', {
+        extend: 'Ext.data.field.Date',
+
+        alias: 'data.field.mhtime',
+
+        convert: function(v, rec){
+            if(Ext.isNumber(v)){
+                return new Date(v);
+            }
+            else if(Ext.isDate(v)){
+                return v;
+            }
+            else if(Ext.isString(v)){
+                return Ext.Date.parse(v, 'H:i:s');
+            }
+            else {
+                return null;
+            }
+        },
+
+        serialize: function(v){
+            return Ext.Date.format(v, 'H:i:s')
+        }
+
+    });
+}());
