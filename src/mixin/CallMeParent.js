99c5f7e8505cc86e9af315526ba5897fc7cff6ed
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-12-06 10:29:19 +0100

    improve callmeparent inherited call chain handling

--- src/mixin/CallMeParent.js
+++ src/mixin/CallMeParent.js
@@ -16,9 +16,30 @@
          */
         callMeParent: function(method, args) {
 
+            //Note:
+            //Improvement ported from MapHive. as usually ;)
+            //setting a calledParent flag on an instance results in it being set already for further calls which is not good
+            //as it indicates a parent call has been made already and leads to unexpected behavior - parent is not called.
+            //therefore need to use a temp object that maps the calls per method, and then wipes them out for further calls
             if(this !== this.superclass){
-                this.superclass[method].apply(this, args);
+
+                if(!this.calledMapCache || !this.calledMapCache[method]){
+                    this.calledMapCache = this.calledMapCache || {};
+                    this.calledMapCache[method] = true;
+                    this.resetCalledMapCache(method);
+                    return this.superclass[method].apply(this, args);
+                }
+                else {
+                    //try to dig deeper
+                    if(Ext.isFunction(this.superclass.superclass[method])){
+                        return this.superclass.superclass[method].apply(this, args);
+                    }
+                }
             }
+            //this is not enough!
+            //if(this !== this.superclass){
+            //    this.superclass[method].apply(this, args);
+            //}
             else {
                 //hmmm, looks like this is the case, the superclas[method] apply keeps on spinning over itself
                 //not entirely sure why, but it looks like it's caused by the way object inheritacne works in ExtJs
@@ -28,6 +49,7 @@
                 }
             }
 
+            //This exposes a problem with setting a prop on an instance described above
             // if(!this.calledParent){
             //     this.calledParent = true;
             //     this.superclass[method].apply(this, args);
@@ -47,6 +69,23 @@
          */
         callMeSuper: function(args) {
             //TODO...
+        },
+        
+        /**
+         * cached map of the called methods in a call chain. gets reset after each call with a little timeout
+         */
+        calledMapCache: null,
+
+        /**
+         * resets call map chain cache
+         * @param method
+         */
+        resetCalledMapCache: function(method){
+            Ext.defer(function(){
+                if(this.calledMapCache && this.calledMapCache[method]){
+                    delete this.calledMapCache[method];
+                }
+            }, 1, this);
         }
     });
 
