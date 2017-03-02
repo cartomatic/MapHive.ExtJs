62b0a4118d49cfe9113fec84293fdda7555d3b5b
Dominik Mikiewicz <dominik@cartomatic.pl>
2017-03-02 12:32:07 +0100

    move in some org ctx dataview utils from dashboard

---
+++ classic/src/mixin/DataViewOrgCtxUtils.js
@@ -0,0 +1,98 @@
+(function(){
+    //Make sure strict mode is on
+    'use strict';
+    
+    /**
+     * handles org ctx context change related functionality for a standard data view.
+     * Created by domin on 27.02.2017.
+     */
+    Ext.define('mh.mixin.DataViewOrgCtxUtils', {
+
+        mixins: [
+            'mh.mixin.ApiMap'
+        ],
+
+        /**
+         * sets up org context related functionality, so a view can adjust its content based on the scoped organisation
+         */
+        setUpOrgContextHandlers: function(){
+            //observing layout evts is the way to know when this comp has been shown
+            this.getView().on('activate', this.onViewShow, this);
+            this.getView().on('deactivate', this.onViewHide, this);
+
+            this.watchGlobal('org::changed', this.onOrgChanged, this);
+        },
+
+        /**
+         * whether or not the component is visible;
+         * IMPORTANT - must be able to observe its own activate / deactivate events! levels of nesting do have importance!
+         */
+        visible: false,
+
+        /**
+         * view deactivate callback; just flags self internally as hidden
+         */
+        onViewHide: function(){
+            this.visible = false;
+        },
+
+        /**
+         * timeout identifier for the internal view show callback
+         */
+        internalViewShowScheduler: null,
+
+        /**
+         * afterlayout callback handler; triggers store reloads and such
+         */
+        onViewShow: function(){
+            this.visible = true;
+
+            //basic flow is: get the org, work out the store url and update store's proxy, trigger store refresh
+
+            var tunnel = this.getTunnelId();
+            this.watchGlobal('org::context', this.orgCtxRetrieved, this, {single: true, tunnel: tunnel});
+            this.fireGlobal('org::getcontext', null, {tunnel: tunnel});
+        },
+
+        /**
+         * org changed callback
+         * @param org
+         */
+        onOrgChanged: function(org){
+            this.reloadStoreOnOrgCtxChange(org);
+        },
+
+        /**
+         * tunnelled org::getcontext callback
+         * @param orgCtx
+         */
+        orgCtxRetrieved: function(orgCtx){
+            this.reloadStoreOnOrgCtxChange(orgCtx.currentOrg);
+        },
+
+        /**
+         * last store url used to load the data
+         */
+        lastDataUrl: null,
+
+        /**
+         * reloads the store for given org on org ctx change or view show. when org ctx has not changed or the view seems to be hidden reload is ignored
+         * @param org
+         */
+        reloadStoreOnOrgCtxChange: function(org){
+
+            if(!this.visible){
+                return;
+            }
+
+            var newUrl = this.getApiEndPoint('organisationUsers').replace(this.getApiMapParentIdentifier(), org.get('uuid'));
+            if(newUrl != this.lastDataUrl){
+                this.lastDataUrl = newUrl;
+
+                this.getViewModel().get('gridstore').getProxy().setUrl(newUrl);
+                this.reloadGrid();
+            }
+        }
+    });
+    
+}());
