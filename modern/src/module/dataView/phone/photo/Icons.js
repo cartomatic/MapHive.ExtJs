13e82bb063949cc261e082450186465b51bee0f8
Dominik Mikiewicz <dominik@cartomatic.pl>
2018-10-14 18:30:07 +0200

    first shot at multi img snapper

---
+++ modern/src/module/dataView/phone/photo/Icons.js
@@ -0,0 +1,22 @@
+//Disable some of the JSLint warnings
+/*global window,console,Ext*/
+(function(){
+    //Make sure strict mode is on
+    'use strict';
+
+    Ext.define('mh.module.dataView.phone.photo.Icons', {
+        singleton: true,
+        requires: [
+            'mh.FontIconsDictionary'
+        ],
+        constructor: function(){
+            mh.FontIconsDictionary.addIcons({
+                mhPhotoSnap: 'x-li li-camera2',
+                mhPhotoDelete: 'x-li li-cross-circle',
+                mhPhotoCancel: 'x-li li-circle-minus',
+                mhPhotoIconDefault: 'x-li li-picture'
+            });
+        }
+    });
+    
+}());
--- modern/src/module/navMenu/NavMenuMobileController.js
+++ modern/src/module/navMenu/NavMenuMobileController.js
@@ -130,7 +130,10 @@
                 //reveal: true
             });
 
-            Ext.Viewport.toggleMenu(this.getView().getMenuSide());
+            //defer a bit so tap ends before action dialog mask pops out. this is to avoid tapping on the dialog mask as it will hide otherwise
+            Ext.defer(function(){
+                Ext.Viewport.toggleMenu(this.getView().getMenuSide());
+            }, 250, this);
         },
 
         /**
