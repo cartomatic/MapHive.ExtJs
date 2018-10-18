ed7a20d1a308334f184ed23f0d3598031d40021a
Dominik Mikiewicz <dominik@cartomatic.pl>
2018-10-18 21:28:37 +0200

    more works on modal edit view wizard

---
+++ modern/src/module/dataView/phone/ModalEditViewWizardModel.js
@@ -0,0 +1,17 @@
+//Disable some of the JSLint warnings
+/*global window,console,Ext*/
+(function(){
+    //Make sure strict mode is on
+    'use strict'
+    
+    Ext.define('mh.module.dataView.phone.ModalEditViewWizardModel', {
+        extend: 'Ext.app.ViewModel',
+        alias: 'viewmodel.mh-phone-modal-edit-view-wizard',
+    
+        stores: {
+        },
+    
+        data: {
+        }
+    });
+}());
