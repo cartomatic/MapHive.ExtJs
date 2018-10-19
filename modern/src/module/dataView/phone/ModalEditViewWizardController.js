501ed6d1b0fa678afaa29c4a435f387551fafb0c
Dominik Mikiewicz <dominik@cartomatic.pl>
2018-10-19 06:52:20 +0200

    minor property naming refactor

--- modern/src/module/dataView/phone/ModalEditViewWizardController.js
+++ modern/src/module/dataView/phone/ModalEditViewWizardController.js
@@ -64,9 +64,9 @@
         },
 
         /**
-         * view activate handler
+         * view show handler
          */
-        onViewActivate: function() {
+        onViewShow: function() {
             //Note: this is buggy, when the first view or all the views are dict list pickers and they have no entries set up yet
             //in such scenario automated jump to next view kicks in and the vies start spinning back and forth.
             //adding a first non-auto-skip view is a temporary solution
