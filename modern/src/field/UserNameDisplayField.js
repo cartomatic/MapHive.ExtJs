0843d86495936ee540c1b99195af7a2f65395789
Dominik Mikiewicz <dominik@cartomatic.pl>
2018-12-06 17:25:50 +0100

    simplistic user name display field

---
+++ modern/src/field/UserNameDisplayField.js
@@ -0,0 +1,49 @@
+//Disable some of the JSLint warnings
+/*global window,console,Ext*/
+(function(){
+    //Make sure strict mode is on
+    'use strict'
+    
+    Ext.define('mh.field.UserNameDisplayField', {
+        extend: 'Ext.field.Display',
+        xtype: 'mh-username-displayfield',
+
+        mixins: [
+            'mh.data.Ajax',
+            'mh.mixin.ApiMap'
+        ],
+
+        config:{
+            emptyValue: '--',
+            loadingMsg: 'Loading...'
+        },
+
+        setUserId: function(userId){
+            //<debug>
+            console.log('UserNameDisplayField - loading user: ', userId);
+            //</debug>
+
+            if(!userId) {
+                this.setHtml(this.getEmptyValue());
+                return;
+            }
+
+            this.setHtml(this.getLoadingMsg());
+
+            this.doGet({
+                url: this.getApiEndPointUrl('userName') + '/' + userId,
+                scope: this,
+                success: this.onUserNameLoadSuccess,
+                failure: this.onUserNameLoadFailure
+            });
+
+        },
+
+        onUserNameLoadFailure: function(){
+            this.setHtml(this.getEmptyValue())
+        },
+        onUserNameLoadSuccess: function(userName){
+            this.setHtml(userName);
+        }
+    });
+}());
