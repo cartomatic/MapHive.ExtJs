cd0f7c885d4c38ad76aa2938eb132573f2fb3f0b
Dominik Mikiewicz <dominik@cartomatic.pl>
2018-12-06 21:58:21 +0100

    minor property value change

--- modern/src/module/dataView/phone/ModalEditViewWizard.js
+++ modern/src/module/dataView/phone/ModalEditViewWizard.js
@@ -48,7 +48,7 @@
             /**
              * whether or not forms should be complete; each view of wizard can prevent scrolling further IF it has a isComplete fn and it returns false
              */
-            enforceCompleteFormsOnViewChange: true,
+            enforceCompleteFormsOnViewChange: false,
 
             /**
              * whether or not should prevent saving until all forms are complete
