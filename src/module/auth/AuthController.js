d1c85c0f4e90321c95464a45d9888dafe4fc59d1
Dominik Mikiewicz <dominik@cartomatic.pl>
2019-04-14 16:00:34 +0200

    make aut controller customizations more flexible

--- src/module/auth/AuthController.js
+++ src/module/auth/AuthController.js
@@ -566,37 +566,43 @@
             //in such case assume an app requires auth and on cancel redirect to home.
 
             if(!currentApp || currentApp.get('requiresAuth')){
-                var me = this;
-
-                //TODO - differnt msg for modern / classic toolkits!
-
-                Ext.Msg.show({
-                    animateTarget: btn,
-                    title: this.getTranslation('cancelAuthWithReloadTitle'),
-                    message: this.getTranslation('cancelAuthWithReloadMsg'),
-                    width: 350,
-                    buttons: Ext.MessageBox.YESNO,
-                    //icon: Ext.MessageBox.QUESTION,
-
-                    //iconCls: 'x-i54c i54c-exit-2',
-
-                    fn: function(btn){
-                        if(btn === 'yes'){
-                            me.reset();
-                            me.getView().hide();
-                            me.fireGlobal('auth::userauthcancel');
-                            me.fireGlobal('root::reloadapp', me.getHomeApp());
-                        }
-                    }
-                });
+                this.cancelAuthForAppsRequiringAuth();
             }
             else {
                 this.reset();
                 this.getView().hide();
                 this.fireGlobal('auth::userauthcancel');
             }
-        }
+        },
 
+        /**
+         * cancels auth for apps requiring auth
+         */
+        cancelAuthForAppsRequiringAuth: function(){
+            var me = this;
+
+            //TODO - different msg for modern / classic toolkits!
+
+            Ext.Msg.show({
+                animateTarget: btn,
+                title: this.getTranslation('cancelAuthWithReloadTitle'),
+                message: this.getTranslation('cancelAuthWithReloadMsg'),
+                width: 350,
+                buttons: Ext.MessageBox.YESNO,
+                //icon: Ext.MessageBox.QUESTION,
+
+                //iconCls: 'x-i54c i54c-exit-2',
+
+                fn: function(btn){
+                    if(btn === 'yes'){
+                        me.reset();
+                        me.getView().hide();
+                        me.fireGlobal('auth::userauthcancel');
+                        me.fireGlobal('root::reloadapp', me.getHomeApp());
+                    }
+                }
+            });
+        }
     });
 
 }());
