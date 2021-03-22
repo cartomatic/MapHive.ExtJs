5595d4f0c75a0f8fa4c1249f056c5a56d20c9772
Dominik Mikiewicz <dominik@cartomatic.pl>
2021-03-22 06:38:24 +0100

    style model extended

--- src/data/model/map/Style.js
+++ src/data/model/map/Style.js
@@ -23,27 +23,80 @@
             {name: 'allObjects', type: 'bool', defaultValue: true, allowNull: false},
             {name: 'filters', type: 'auto', defaultValue: null, allowNull: true},
 
+            //Polys
             {name: 'poly', type: 'bool', defaultValue: false, allowNull: false},
             {name: 'polyFillColor', type: 'string', defaultValue: '80a8ea55', allowNull: true},
             {name: 'polyStrokeColor', type: 'string', defaultValue: '4b4ceaff', allowNull: true},
             {name: 'polyStrokeWidth', type: 'number', defaultValue: 1, allowNull: true},
+            //Poly labels
+            {name: 'polyLabels', type: 'bool', defaultValue: false, allowNull: false},
+            {name: 'polyLabelField', type: 'string', allowNull: true},
+            {name: 'polyLabelVisibilityScaleMin', type: 'number', defaultValue: null, allowNull: true},
+            {name: 'polyLabelVisibilityScaleMax', type: 'number', defaultValue: null, allowNull: true},
+            {name: 'polyLabelDeclutter', type: 'bool', defaultValue: false, allowNull: false},
 
-            {name: 'line', type: 'bool', defaultValue: false, allowNull: false},
+            {name: 'polyLabelWrap', type: 'string', defaultValue: 'wrap', allowNull: true},
+            {name: 'polyLabelAlign', type: 'string', defaultValue: 'center', allowNull: true},
+            {name: 'polyLabelBaseLine', type: 'string', defaultValue: 'middle', allowNull: true},
+            {name: 'polyLabelRotation', type: 'number', defaultValue: 0},
+            {name: 'polyLabelFont', type: 'string', defaultValue: 'Arial', allowNull: true},
+            {name: 'polyLabelFontStyle', type: 'string', defaultValue: 'Normal', allowNull: true},
+            {name: 'polyLabelPlacement', type: 'string', defaultValue: 'point', allowNull: true},
+            {name: 'polyLabelMaxAngle', type: 'number', defaultValue: 45},
+            {name: 'polyLabelOverflow', type: 'bool', defaultValue: false, allowNull: false},
+
+            {name: 'polyLabelSize', type: 'number', defaultValue: 10},
+            {name: 'polyLabelLineHeight', type: 'number', defaultValue: 1},
+            {name: 'polyLabelOffsetX', type: 'number', defaultValue: 0},
+            {name: 'polyLabelOffsetY', type: 'number', defaultValue: 0},
+            {name: 'polyLabelColor', type: 'string', defaultValue: '000000ff', allowNull: false},
+            {name: 'polyLabelOutlineColor', type: 'string', defaultValue: 'ffffff00', allowNull: false},
+            {name: 'polyLabelOutlineWidth', type: 'number', defaultValue: 0},
+
+
+            //Lines
+            {name: 'poly', type: 'bool', defaultValue: false, allowNull: false},
             {name: 'lineStrokeColor', type: 'string', defaultValue: '4b4ceaff', allowNull: true},
             {name: 'lineStrokeWidth', type: 'number', defaultValue: 1, allowNull: true},
+            //Line labels
+            {name: 'lineLabels', type: 'bool', defaultValue: false, allowNull: false},
+            {name: 'lineLabelField', type: 'string', allowNull: true},
+            {name: 'polyLabelVisibilityScaleMin', type: 'number', defaultValue: null, allowNull: true},
+            {name: 'polyLabelVisibilityScaleMax', type: 'number', defaultValue: null, allowNull: true},
+            {name: 'polyLabelDeclutter', type: 'bool', defaultValue: false, allowNull: false},
 
+            {name: 'polyLabelWrap', type: 'string', defaultValue: 'wrap', allowNull: true},
+            {name: 'polyLabelAlign', type: 'string', defaultValue: 'center', allowNull: true},
+            {name: 'polyLabelBaseLine', type: 'string', defaultValue: 'middle', allowNull: true},
+            {name: 'polyLabelRotation', type: 'number', defaultValue: 0},
+            {name: 'polyLabelFont', type: 'string', defaultValue: 'Arial', allowNull: true},
+            {name: 'polyLabelFontStyle', type: 'string', defaultValue: 'Normal', allowNull: true},
+            {name: 'polyLabelPlacement', type: 'string', defaultValue: 'point', allowNull: true},
+            {name: 'polyLabelMaxAngle', type: 'number', defaultValue: 45},
+            {name: 'polyLabelOverflow', type: 'bool', defaultValue: false, allowNull: false},
 
+            {name: 'polyLabelSize', type: 'number', defaultValue: 10},
+            {name: 'polyLabelLineHeight', type: 'number', defaultValue: 1},
+            {name: 'polyLabelOffsetX', type: 'number', defaultValue: 0},
+            {name: 'polyLabelOffsetY', type: 'number', defaultValue: 0},
+            {name: 'polyLabelColor', type: 'string', defaultValue: '000000ff', allowNull: false},
+            {name: 'polyLabelOutlineColor', type: 'string', defaultValue: 'ffffff00', allowNull: false},
+            {name: 'polyLabelOutlineWidth', type: 'number', defaultValue: 0},
+
+            //Points
             {name: 'point', type: 'bool', defaultValue: false, allowNull: false},
             {name: 'pointSize', type: 'number', defaultValue: 5, allowNull: true},
             {name: 'pointShape', type: 'string', defaultValue: 'circle', allowNull: true},
             {name: 'pointFillColor', type: 'string', defaultValue: '80a8ea55', allowNull: true},
             {name: 'pointStrokeColor', type: 'string', defaultValue: '4b4ceaff', allowNull: true},
             {name: 'pointStrokeWidth', type: 'number', defaultValue: 1, allowNull: true},
-
+            //Point labels
             {name: 'pointLabels', type: 'bool', defaultValue: false, allowNull: false},
             {name: 'pointLabelField', type: 'string', allowNull: true},
             {name: 'pointLabelVisibilityScaleMin', type: 'number', defaultValue: null, allowNull: true},
             {name: 'pointLabelVisibilityScaleMax', type: 'number', defaultValue: null, allowNull: true},
+            {name: 'pointLabelDeclutter', type: 'bool', defaultValue: false, allowNull: false},
+
             {name: 'pointLabelWrap', type: 'string', defaultValue: 'wrap', allowNull: true},
             {name: 'pointLabelAlign', type: 'string', defaultValue: 'center', allowNull: true},
             {name: 'pointLabelBaseLine', type: 'string', defaultValue: 'middle', allowNull: true},
