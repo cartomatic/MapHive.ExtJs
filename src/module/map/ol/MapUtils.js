97e1e952559006f30f8a430a1882f3446c869aad
Dominik Mikiewicz <dominik@cartomatic.pl>
2025-03-22 13:32:51 +0100

    print ol map to img

--- src/module/map/ol/MapUtils.js
+++ src/module/map/ol/MapUtils.js
@@ -125,6 +125,73 @@
             ctx.drawImage(tmpC, 0, 0);
 
             ctx.globalAlpha = a;
+        },
+
+        /**
+         * prints a map to pdf
+         * @private
+         * @param map
+         * @param cfg
+         * @param cfg.resolution resolution expressed in dpi
+         * @param cfg.size printout dimensions expressed in mm: [height, width]
+         * @param cfg.callback a callback to call when a printout is ready - dataUrl is passed as the param
+         */
+        mapPrint: function(map, cfg){
+
+            if(!Ext.isFunction(cfg.callback)){
+                console.error('Map printer requires a callback to be provided');
+                return;
+            }
+
+            const width = Math.round((cfg.size[0] * cfg.resolution) / 25.4);
+            const height = Math.round((cfg.size[1] * cfg.resolution) / 25.4);
+            const size = map.getSize();
+            const viewResolution = map.getView().getResolution();
+
+            map.once('rendercomplete', function () {
+                const mapCanvas = document.createElement('canvas');
+                mapCanvas.width = width;
+                mapCanvas.height = height;
+                const mapContext = mapCanvas.getContext('2d');
+                Array.prototype.forEach.call(
+                    document.querySelectorAll('.ol-layer canvas'),
+                    function (canvas) {
+                        if (canvas.width > 0) {
+                            const opacity = canvas.parentNode.style.opacity;
+                            mapContext.globalAlpha = opacity === '' ? 1 : Number(opacity);
+                            const transform = canvas.style.transform;
+                            // Get the transform parameters from the style's transform matrix
+                            const matrix = transform
+                                .match(/^matrix\(([^\(]*)\)$/)[1]
+                                .split(',')
+                                .map(Number);
+                            // Apply the transform to the export map context
+                            CanvasRenderingContext2D.prototype.setTransform.apply(
+                                mapContext,
+                                matrix,
+                            );
+                            mapContext.drawImage(canvas, 0, 0);
+                        }
+                    },
+                );
+                mapContext.globalAlpha = 1;
+                mapContext.setTransform(1, 0, 0, 1, 0, 0);
+
+                //let the map return to its orig size before assembling a pdf - render canvas in mem first, then resize map, then do pdf
+                const imgData = mapCanvas.toDataURL('image/jpeg');
+
+                // Reset original map size
+                map.setSize(size);
+                map.getView().setResolution(viewResolution);
+
+                cfg.callback(imgData);
+            });
+
+            // Set print size
+            const printSize = [width, height];
+            map.setSize(printSize);
+            const scaling = Math.min(width / size[0], height / size[1]);
+            map.getView().setResolution(viewResolution / scaling);
         }
 
     });
